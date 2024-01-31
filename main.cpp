6f25b015d340defce2ebf772e9e7aaeac61973b2
MMariasik <251185@edu.p.lodz.pl>
2024-01-31 13:17:51 +0100

    dodanie funkcji policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+int policz(int a , int b)
+{
+    return a + b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "W perwszym branchu " << policz(2, 4) << std::endl;
     return 0;
 }
